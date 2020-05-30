2217d767201ad6cea5a411980fef6b06840823c7
Sachin Kumar Mishra <mishrasachin811@gmail.com>
2020-05-30 09:07:47 +0530

    Add files via upload

--- todolist.js
+++ todolist.js
@@ -1,16 +1,22 @@
 var todos = [];
-var input = prompt("choose a option among 3. What do you want to do?");
+// asking for user input
+var input = prompt("choose an option among 3. \n 1. new - add new task in list \n 2. list - list the added task \n 3. quit - quit the app \n What do you want to do?");
 
-
+//if user dosen't want to quit the app
 while(input != "quit"){
+    // if user want to add a   new task  
     if ( input == "new") {
         var newtodo = prompt("Add a task which you want to do");
-        todos = todos + " " +newtodo;
+        todos = todos + "\n" +newtodo;
     }
+    // if user want to list out the task saved in list
     else if(input == "list"){
+        alert( "These are the pending task in your to do list \n "+todos);
         console.log(todos);
 
     }
-    var input = prompt("choose a option among 3. What do you want to do?");
+    // asking again if user want to do something
+    var input = prompt("choose an option among 3. \n 1. new - add new task in list \n 2. list - list the added task \n 3. quit - quit the app \n What do you want to do?");
 }
+//if user chooses quit
 alert("You quit the app");
